510d312521af9979b0e78a9174962b4d35c0793c
Ian J <ianbennettjohnson@gmail.com>
2020-06-25 11:37:11 -0700

    Got gradient to show

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Grid, Cell } from 'react-mdl';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div style={{width: "100%", margin: "auto"}}>
+            <Grid className="home-grid">
+                <Cell col={12}></Cell>
+            </Grid>
         </div>
     );
 }
