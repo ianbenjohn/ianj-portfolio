d424f77b49dfa0a7c371f9e57e41359a4959ffba
Ian J <ianbennettjohnson@gmail.com>
2020-06-24 19:39:06 -0700

    home page responding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Home from './pages/Home';
 import './App.css';
 
 
 function App() {
   return (
-    <div>
-      <h1>Very Nice. My Wife</h1>
-    </div>
+    <Home />
   );
 }
 
