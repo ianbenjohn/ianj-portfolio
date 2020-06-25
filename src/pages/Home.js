a0dfa7efd5c4fb10d479c269d8fb0220f1088bea
Ian J <ianbennettjohnson@gmail.com>
2020-06-25 12:03:28 -0700

    Got image in

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { Grid, Cell } from 'react-mdl';
 import './Home.css';
 
+
 const Home = () => {
+    const imgStyle = {
+        height: "250px",
+        borderRadius: "20%",
+        borderStyle: "solid"
+    }
+    
     return (
         <div style={{width: "100%", margin: "auto"}}>
             <Grid className="home-grid">
-                <Cell col={12}></Cell>
+                <Cell col={12}>
+                    <img
+                        src= "../img/ian-image.jpg"
+                        alt= "myFace"
+                        style = {imgStyle}
+                    />
+                </Cell>
             </Grid>
         </div>
     );
