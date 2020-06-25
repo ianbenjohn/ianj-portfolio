11351eecfd8f6a2933fea277d0939b8ff4dbf35a
Ian J <ianbennettjohnson@gmail.com>
2020-06-25 13:19:33 -0700

    putting content and styling on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,60 @@
 import React from 'react';
 import { Grid, Cell } from 'react-mdl';
-import './Home.css';
+import '../App.css'
+
+
 
 
 const Home = () => {
+    //Styles for Home Page
     const imgStyle = {
+        borderRadius: "40%",
         height: "250px",
-        borderRadius: "20%",
-        borderStyle: "solid"
+        paddingTop: "10px",
     }
-    
+    const homeGrid = {
+        
+    }
+    const bannerStyle = {
+        backgroundColor: "black",
+        opacity: ".8",
+        width: "75%",
+        margin: "auto",
+        borderRadius: "10px",
+    }
+    const h1Style = {
+        fontSize: "60px",
+        fontWeight: "bold",
+        color: "cornsilk",
+    }
+    const hrStyle = {
+        borderTop: "5px dotted white",
+        width: "50%",
+        margin: "auto"
+    }
+    const pStyle = {
+        color: "cornsilk",
+        fontSize: "20px",
+        padding: "1em"
+    }
+
     return (
         <div style={{width: "100%", margin: "auto"}}>
-            <Grid className="home-grid">
+            <Grid style={homeGrid}>
                 <Cell col={12}>
                     <img
                         src= "../img/ian-image.jpg"
                         alt= "myFace"
                         style = {imgStyle}
                     />
+
+                    <div style = {bannerStyle}>
+                        <h1 style={h1Style}>Legendary Full-Stack Developer</h1>
+                        
+                    <hr style={hrStyle}/>
+                        <p style={pStyle}>HTML/CSS | JavaScript | ReactJS | NodeJS | Express | MongoDB | MySQL</p>
+
+                    </div>
                 </Cell>
             </Grid>
         </div>
@@ -26,3 +62,4 @@ const Home = () => {
 }
 
 export default Home;
+
