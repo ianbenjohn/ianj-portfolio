1ac2d0e16a9a262f8219382fd7ee6bae762a4235
Ian J <ianbennettjohnson@gmail.com>
2020-06-29 20:25:37 -0700

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,39 +1,94 @@
 import React from 'react';
-import{ Tabs, Tab } from 'react-mdl';
+import{ Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
 import Nav from '../components/Nav';
 
 
+const firstProject = {
+    margin: '70px 0px 30px 0px'
+}
 
-class Projects extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { activeTab: 2 };
-    }
 
-    render() {
-        const categoryTabs = {
-            color: "pink",
-            paddingTop: "66px",
-            border: "5px solid black",
-            height: "100vh"
-        
-        }
-        return (
-        <div>
-            <Nav />
-            <div style={categoryTabs}>
-                
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab>School Projects</Tab>
-                    <Tab>Individual Projects</Tab>
-                </Tabs>
-                <section>
-                    <div className="content">Content for the tab: {this.state.activeTab}</div>
-                </section>
-            </div>   
-        </div> 
-        );
-    }
+
+const Projects = () => {
+    return(
+        <div style={{display: "flex", flexWrap: 'wrap', justifyContent: "space-evenly"}}>
+
+        <div style={firstProject}>
+            <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Employee Manager</CardTitle>
+                     <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenan convallis.
+                    </CardText>
+                <CardActions border>
+                     <Button colored>View Updates</Button>
+                </CardActions>
+            </Card>
+        </div>
+
+        <div className="secondProject">
+            <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                     <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenan convallis.
+                    </CardText>
+                <CardActions border>
+                     <Button colored>View Updates</Button>
+                </CardActions>
+            </Card>
+        </div>
+        <div className="thirdProject">
+            <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                     <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenan convallis.
+                    </CardText>
+                <CardActions border>
+                     <Button colored>View Updates</Button>
+                </CardActions>
+            </Card>
+        </div>
+        <div className="fourthProject">
+            <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                     <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenan convallis.
+                    </CardText>
+                <CardActions border>
+                     <Button colored>View Updates</Button>
+                </CardActions>
+            </Card>
+        </div>
+        <div className="fifthProject">
+            <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                     <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenan convallis.
+                    </CardText>
+                <CardActions border>
+                     <Button colored>View Updates</Button>
+                </CardActions>
+            </Card>
+        </div>
+        <div className="sixthProject">
+            <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+                <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>Update</CardTitle>
+                     <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenan convallis.
+                    </CardText>
+                <CardActions border>
+                     <Button colored>View Updates</Button>
+                </CardActions>
+            </Card>
+        </div>
+        </div>
+
+    );
 }
 
 export default Projects;
