e9a3431e2d05619860b69f8aa0bf0f9ecf198dcb
Ian J <ianbennettjohnson@gmail.com>
2020-06-30 18:00:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ let app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
 const port = process.env.PORT || '8080';
 app.set('port', port);
 
