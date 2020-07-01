673fcb57e0ebd7ec1efe410e868b8cd35e494fe5
Ian J <ianbennettjohnson@gmail.com>
2020-06-30 18:19:56 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const http = require('http');
-const path = require('path');
+// const http = require('http');
+// const path = require('path');
 
-let app = express();
+const app = express();
+const PORT = process.env.PORT || '8080';
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("./build"));
   }
 
-const port = process.env.PORT || '8080';
-app.set('port', port);
+// app.set('port', port);
 
-const server = http.createServer(app);
-server.listen(port, () => console.log(`running on localhost`));
+// const server = http.createServer(app);
+// server.listen(port, () => console.log(`running on localhost`));
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
